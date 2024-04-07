419f2cb66e8d8d16a31dbb9cebfd1d0a6903c309
David Roman <hello@davidroman.io>
2024-04-07 13:54:35 -0400

    adding comfy

---
+++ _example/basic/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-sql/v3/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/davidroman0O/comfylite3"
+	comfymill "github.com/davidroman0O/watermill-comfylite3"
+)
+
+func main() {
+
+	db, err := comfymill.NewDatabase(
+		comfylite3.WithMemory(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	logger := watermill.NewStdLogger(false, false)
+
+	subscriber, err := sql.NewSubscriber(
+		db,
+		sql.SubscriberConfig{
+			SchemaAdapter:    comfymill.DefaultSQLite3Schema{},
+			OffsetsAdapter:   comfymill.DefaultSQLite3OffsetsAdapter{},
+			InitializeSchema: true,
+		},
+		logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "example_topic")
+	if err != nil {
+		panic(err)
+	}
+
+	go process(messages)
+
+	publisher, err := sql.NewPublisher(
+		db,
+		sql.PublisherConfig{
+			SchemaAdapter: comfymill.DefaultSQLite3Schema{},
+		},
+		logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	publishMessages(publisher)
+}
+
+func publishMessages(publisher message.Publisher) {
+	for {
+		msg := message.NewMessage(watermill.NewUUID(), []byte(`{"message": "Hello, world!"}`))
+
+		if err := publisher.Publish("example_topic", msg); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
+}
--- go.mod
+++ go.mod
@@ -5,13 +5,16 @@ go 1.22.0
 require (
 	github.com/ThreeDotsLabs/watermill v1.3.5
 	github.com/ThreeDotsLabs/watermill-sql/v3 v3.0.1
+	github.com/davidroman0O/comfylite3 v0.0.0-20240407174050-258b1d250c5c
 )
 
 require (
-	github.com/davidroman0O/comfylite3 v0.0.0-20240407155802-7f6310b61f3c // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
